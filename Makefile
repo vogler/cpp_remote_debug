8151f44c6de1e4d4c71ac240c3853de3bfe250fb
Ralf Vogler <ralf.vogler@gmail.com>
2019-07-08 23:20:36 +0200

    cmd `break` to set breakpoint on line, exit, label for each line

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+run:
+	c++ prog.cpp && ./a.out
--- dbg.cpp
+++ dbg.cpp
@@ -9,24 +9,35 @@ string cmd;
 struct {
   bool paused;
   unsigned int line;
+  unsigned int breakpoint;
 } dbg;
 
-#define DBG dbg.line = __LINE__; while(dbg.paused){}
+#define C_(A,B) A##B
+#define C(A,B) C_(A,B)
+#define DBG C(L,__LINE__): dbg.line = __LINE__; dbg.paused |= dbg.line == dbg.breakpoint; if(dbg.paused) cout << "execution paused at line " << dbg.line << endl; while(dbg.paused) sleep(1);
 
 void *f(void *arg){
-  while(getline(cin, cmd)){
+  while(getline(cin, cmd)){ // read until endl
     if(cmd == "pause"){
-      dbg.paused = !dbg.paused;
-      cout << "execution paused at line " << dbg.line << endl;
+      dbg.paused = true;
+    } else if(cmd == "resume"){
+      dbg.paused = false;
+    } else if(cmd == "break"){ // e.g. break 42
+      cout << "on which line?" << endl;
+      cin >> ws >> dbg.breakpoint;
+      cin.ignore();
+      cout << "breakpoint set at line " << dbg.breakpoint << endl;
+    } else if(cmd == "exit"){
+      exit(0);
     } else {
       cout << "unknown dbg cmd: " << cmd << endl;
     }
   }
-  return NULL;
+  return nullptr;
 }
 
 int dbg_start(){
   pthread_t t;
-  pthread_create(&t, NULL, f, NULL);
+  pthread_create(&t, nullptr, f, nullptr);
   return 0;
 }
